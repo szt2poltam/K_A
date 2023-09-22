cb095c3f9858779d31878bbf279d3a01fd6e8f65
Polyákovich Tamás <115459551+szt2poltam@users.noreply.github.com>
2023-09-22 08:38:43 +0200

    Add files via upload

---
+++ DrawDiamond.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+class Diamond {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Please enter the diamond height: ");
+        int height = scanner.nextInt();
+
+        for (int i = 1; i <= height; i++) {
+            for (int j = height - i; j > 0; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = height - 1; i >= 1; i--) {
+            for (int j = height - i; j > 0; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
