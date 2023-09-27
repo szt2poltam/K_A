98f466391e2010d13f93d83f5b15f685f226cc12
Polyákovich Tamás <115459551+szt2poltam@users.noreply.github.com>
2023-09-27 20:01:27 +0200

    Add files via upload

---
+++ FavoriteNumber.java
@@ -0,0 +1,7 @@
+public class FavoriteNumber {
+    public static void main(String[] args) {
+        int favoriteNumber = 8;  // Your favorite number
+
+        System.out.println("My favorite number is: " + favoriteNumber);
+    }
+}
